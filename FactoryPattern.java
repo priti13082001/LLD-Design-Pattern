ebf4018c35156bfef85986de31a5f729f3b94dcf
odcadmin <odc.admin@bajajfinserv.in.n2p>
2025-03-23 20:08:00 +0530

    commit for my first LLD Design pattern

---
+++ FactoryPattern.java
@@ -0,0 +1,54 @@
+//factory desing pattarn 
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class FactoryPattern
+{
+	public static void main(String[] args) {
+	    shapeFactory shapeobj=new shapeFactory();
+	   shape s=shapeobj.getShape("SQUARE");
+	    s.draw();
+	
+	}
+}
+interface shape{
+   public void draw();
+}
+ class circle implements shape{
+    @Override
+    public void draw(){
+        System.out.println("circle object is create");
+    }
+}
+ class square implements shape{
+    @Override
+    public void draw(){
+        System.out.println("square object is create");
+    }
+}
+ class rectangle implements shape{
+    @Override
+    public void draw(){
+        System.out.println("rectangle object is create");
+    }
+}
+class shapeFactory{
+    
+        shape getShape(String input){
+            switch(input){
+            case "CIRCLE":
+               return new circle();
+            case "SQUARE":
+               return new square();
+            default:
+            return null;
+            }
+        }
+         
+}
+
